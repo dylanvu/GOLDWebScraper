9470b9f8a24da8a5cee2dbd205ba5b440ed4d96b
Dylan Vu <75911565+vu-dylan@users.noreply.github.com>
2021-01-08 20:49:20 -0800

    Added Development Ideas

--- main.py
+++ main.py
@@ -174,16 +174,3 @@ for num in range(0, len(department_columns)):
 
 print("Script Complete!")
 scraped_course_wb.save("Output Sheet.xlsx")
-
-# what's next? Ideas for development?
-    # figure out how to generalize accessing the path to the template file? make it more user friendly, somehow. May be able to only be done after converting to a .exe
-    # figure out how to sort and format through all of this data as there will be a lot of it
-        # may be a task for excel and VBA? Inject VBA code and run it using PyWin32?
-            # Automate the creation of charts for each class?
-            # Create new worksheets per department with the classes on the left, times on the top?
-    # repeat the webscraper every hour between a specified time frame
-        # Figure out how to update an existing Excel document
-    # Optimize the single course scrape algorithim?
-    # get rid of the .env file, instead integrate gold username and password directly into Excel sheet?
-    # export into a .exe file?
-        # eventually create a simple GUI, where you can input special template excel file and set a time range to scrape. Have that run in the background and be easily cancelled at any time?
